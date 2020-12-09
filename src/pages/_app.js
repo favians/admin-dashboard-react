633f3b78ae40bdcbcb93a9eb73ffca36720ea025
faviansyah <faviansyah@alterra.id>
2020-12-09 21:32:49 +0700

    [2020/12/09]-redux management

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,14 @@
 import 'resources/styles/globals.css'
+import { initializeStore } from 'reduxjs';
+import { Provider } from 'react-redux';
 
+const store = initializeStore();
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Provider store={store}>
+      <Component {...pageProps} />
+    </Provider>
+  )
 }
 
 export default MyApp
