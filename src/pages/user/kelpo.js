5a4af7e2dd58b74768929801a5de96bbfd2b42d9
faviansyah <faviansyah@alterra.id>
2020-12-27 20:53:39 +0700

    [2020/12/27]-Add Image Sample

--- src/pages/user/kelpo.js
+++ src/pages/user/kelpo.js
@@ -1,9 +1,14 @@
 import Kelpo from 'components/user/kelpo';
 import styles from 'resources/styles/Home.module.css';
+import Image from 'next/image';
 
 export default function KelpoUser() {
   return (
     <div className={styles.container}>
+      <Image
+        src="/images/bayi-gondrong.jpg"
+        alt="Picture of the author"
+      />
       <Kelpo title="kelpo page only" color="red" />
     </div>
   );
